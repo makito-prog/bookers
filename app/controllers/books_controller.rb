508cd34a21d1e423d6a38c8c16c9a9e9698c4cf1
makito-prog <147535008+makito-prog@users.noreply.github.com>
2024-09-20 01:06:05 +0000

    Finish create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,20 @@ class BooksController < ApplicationController
   def index
   end
   
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+  
   def show
   end
   
   def eidt
   end
+  
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
